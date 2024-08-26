4b4380e4c23f94c0a8d434f6ad3f0b3d25ef4a91
Joshua Flowers <flowers.joshuac@gmail.com>
2024-08-26 01:32:14 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple chat application written in C, so that I can learn how to use mutexes and sockets.
---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+FILES=$(find src/ -name "*.c")
+LIB_FILES=$(find lib/ -name "*.o")
+
+clang -o bin/chat -I src/headers -I lib/headers $LIB_FILES $FILES
