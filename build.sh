c6439a1b22f3d1d0cd8cc578261f0267065da4f3
Joshua Flowers <flowers.joshuac@gmail.com>
2024-10-18 14:32:51 -0500

    Updated build script

--- build.sh
+++ build.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-FILES=$(find src/ -name "*.c")
-LIB_FILES=$(find lib/ -name "*.o")
+if test ! -e "bin"; then
+	mkdir -p "bin/release"
+	mkdir -p "bin/debug"
+fi
 
-clang -o bin/chat -I src/headers -I lib/headers $LIB_FILES $FILES
-clang -O0 -g -o bin/chat_dbg -I src/headers -I lib/headers $LIB_FILES $FILES
+FILES=$(find "src/" -name "*.c")
+LIB_FILES=$(find "lib/" -name "*.o")
+
+COMPILER=clang
+
+if ! command -v clang; then
+	COMPILER=gcc
+fi
+
+BINARY_NAME="chat"
+
+$COMPILER -I "src/headers" -I "lib/headers" -O3 $LIB_FILES $FILES -o "bin/release/$BINARY_NAME"
+$COMPILER -I "src/headers" -I "lib/headers" -O0 -g $LIB_FILES $FILES -o "bin/debug/$BINARY_NAME"
